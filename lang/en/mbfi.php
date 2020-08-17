08d2c1e3f472085eab0d22686d6a07b33457a932
ortegacarlos <carloslco246@gmail.com>
2020-08-16 19:17:14 -0500

    Agregando nuevas cadenas de texto

--- lang/en/mbfi.php
+++ lang/en/mbfi.php
@@ -38,14 +38,30 @@ $string['mbfiname_help'] = 'Digite el nombre identificador para la nueva activid
 $string['nonewmodules'] = 'No hay intancias de la actividad Resultados BFI.';
 
 // Add string by form
-$string['feedbackar'] = 'Seleccione una encuesta: ';
-$string['feedbackar_help'] = 'Escoja la encuesta para medir los rasgos de personalidad de los individuos que han diligenciado completamente la misma.';
+$string['datasource'] = 'Seleccione una opción';
+$string['datasource_help'] = 'Seleccione una opción si desea procesar las respuestas obtenidas del módulo "Resultados BFI", de lo contrario seleccione la opción "Subir archivo" para procesar sus respuestas.';
+
+$string['feedback'] = 'Encuesta previa';
+$string['uploadfile'] = 'Subir archivo';
+
+$string['feedbackar'] = 'Seleccione una encuesta';
+$string['feedbackar_help'] = 'Escoja la encuesta para procesar sus respuestas y medir los rasgos de personalidad de los individuos que han diligenciado completamente la misma.';
+
+$string['userfile'] = 'Archivo de datos';
+$string['userfile_help'] = 'Campo para cargar un archivo de texto plano, con valores separados por comas (.csv), que almacena los datos de una actividad tipo Encuesta del "Big-Five Inventory".';
 
 // Add string errors
-$string['err_recordsfeedback'] = 'No se encontraron encuestas en el curso.';
+//$string['err_recordsfeedback'] = 'No se encontraron encuestas en el curso.';
 $string['err_feedbackcompleted'] = 'Ningún individuo ha completado la encuesta "{$a->name}".';
 $string['err_answerscounting'] = 'El usuario "{$a->fullname}" aún no ha completado el cuestionario.';
-$string['err_nonefeedback'] = 'No se ha seleccionado una encuesta';
+$string['err_nonefeedback'] = 'No se ha seleccionado una encuesta.';
+$string['err_savefile'] = 'Error al guardar el archivo o  no se cargó ningún archivo previamente.';
+$string['err_deletefile'] = 'Error al eliminar el archivo.';
+$string['err_readfile'] = 'Error al leer el archivo.';
+$string['err_checkfile'] = 'Revise las inconsistencias encontradas en el archivo.';
+$string['err_checkparameters'] = 'Hay inconsistencias en la línea {$a->number}. El número de respuestas guardadas en el archivo no coincide.';
+$string['err_organizefiledata'] = 'Se ha producido un error al organizar los datos del archivo.';
+$string['err_calculatedimensions'] = 'Se ha producido un error al calcular las dimensiones de cada individuo.';
 
 // Add string by view
 $string['fullnamehd'] = 'Nombre(s)/Apellido(s)';
